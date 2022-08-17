ca377bab1b7b1a36d33eccef6dfbac47ca3d3ff6
djembissi <dbricebouguet@yahoo.fr>
2022-08-17 23:54:53 +0000

    my first commit

---
+++ deploy.java
@@ -0,0 +1,7 @@
+#!/bin/bash
+#write a script to create users in linux sever
+#run the script with sudo access
+echo "please enter the user name you want to create"
+sudo adduser $username
+read username
+
